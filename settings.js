82c9ebb9f9da99e121fb5794fa744549bae62e84
Muhammad Waseem <waseemuxui@gmail.com>
2025-04-07 01:09:16 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Knight',
+  author: 'Mr Waseem',
+  botName: "Shafi IT Solutions",
+  botOwner: 'Mr Waseem UXUi', // Your name
+  ownerNumber: '923341122126', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
